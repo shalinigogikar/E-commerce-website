d762d830192ec1ea571a8f7d00ab8f3fd9794eb1
Shalini Gogikar <shalinigogikar23@gmail.com>
2025-02-14 12:07:31 +0530

    context is imported

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React,{useContext, useState}from "react";
 import {Button} from "react-bootstrap";
 import { Container,Navbar,Nav } from "react-bootstrap";
 import ProductList from "./components/ProductList";
 import classes from "./components/Style.module.css";
 import Footer from "./components/Footer";
+import Cart from "./components/Cart";
+import  { CartContext } from "./components/CartContext";
 function App() {
+  const [showCart, setShowCart] = useState(false);
+  const {cart}=useContext(CartContext);
   return (
     <>
     <Navbar bg="dark" variant="dark" expand="md">
@@ -14,11 +18,12 @@ function App() {
       <Nav.Link href="#">Store</Nav.Link>
       <Nav.Link href="#">About</Nav.Link>
       </Nav>
-      <Button>Cart</Button>
+      <Button onClick={() => setShowCart(true)}>Cart{cart.length}</Button>
       </Container>
     </Navbar>
     <header className={classes.header}>The Generics</header>
     <ProductList/>
+    <Cart show={showCart} handleClose={() => setShowCart(false)} />
     <Footer/>
   </>
   );
