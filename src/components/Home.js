acd34cf8b6a576461490dcc93a52351ec8a80e43
Shalini Gogikar <shalinigogikar23@gmail.com>
2025-02-15 18:24:40 +0530

    Home elements are added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,31 @@
 import React from "react";
-import { Container } from "react-bootstrap";
-
+import { Container,Button,Table } from "react-bootstrap";
 const Home = () => {
+  const tourData = [
+    { date: "JUL 16", location: "DETROIT, MI", venue: "DTE ENERGY MUSIC THEATRE" },
+    { date: "JUL 19", location: "TORONTO, ON", venue: "BUDWEISER STAGE" },
+    { date: "JUL 22", location: "BRISTOW, VA", venue: "JIGGY LUBE LIVE" },
+    { date: "JUL 29", location: "PHOENIX, AZ", venue: "AK-CHIN PAVILION" },
+    { date: "AUG 2", location: "LAS VEGAS, NV", venue: "T-MOBILE ARENA" },
+    { date: "AUG 7", location: "CONCORD, CA", venue: "CONCORD PAVILION" },
+  ];
   return (
     <Container className="mt-5 text-center">
-      <h1>Welcome to The Generics</h1>
-      <p>Your one-stop shop for amazing products!</p>
+      <h2>Tours</h2>
+  <Table>
+      <tbody>
+          {tourData.map((tour, index) => (
+            <tr key={index}>
+              <td>{tour.date}</td>
+              <td>{tour.location}</td>
+              <td>{tour.venue}</td>
+              <td>
+                <Button variant="primary">BUY TICKETS</Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </Container>
   );
 };
