b92e730d5aa61e8640561c6423d2cb1013422b99
Shalini Gogikar <shalinigogikar23@gmail.com>
2025-02-15 15:11:49 +0530

    home page is created

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Home = () => {
+  return (
+    <Container className="mt-5 text-center">
+      <h1>Welcome to The Generics</h1>
+      <p>Your one-stop shop for amazing products!</p>
+    </Container>
+  );
+};
+export default Home;
