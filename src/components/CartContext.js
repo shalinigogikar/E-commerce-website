311d1105a8f8b401c5ebb78d5129fcdde7e20226
Shalini Gogikar <shalinigogikar23@gmail.com>
2025-02-15 12:05:11 +0530

    cartelements are added and quantity number functionality is included

--- src/components/CartContext.js
+++ src/components/CartContext.js
@@ -1,12 +1,46 @@
 import React,{createContext,useState} from "react";
 export const CartContext=createContext();
 const CartProvider=({children})=>{
-const [cart,setCart]=useState([]);
+const [cart,setCart]=useState([
+    {
+        title: "Colors",
+        price: 100,
+        imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+        quantity: 2,
+      },
+      {
+        title: "Black and white Colors",
+        price: 50,
+        imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+        quantity: 3,
+      },
+      {
+        title: "Yellow and Black Colors",
+        price: 70,
+        imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+        quantity: 1,
+      },
+]);
 const addToCart=(product)=>{
-    setCart((prev)=>[...prev,product]);
-};
+    setCart((prev)=>{
+        const existItemIndex=prev.findIndex((item)=>item.title===product.title);
+        if(existItemIndex!==-1){
+            const updatedItem=[...prev];
+            updatedItem[existItemIndex].quantity+=1;
+            return updatedItem;}
+            else{
+               return[...prev,{...product,quantity:1}];
+}
+    });
+}
+const removeFromCart = (title) => {
+    setCart((prevCart) => prevCart
+      .map((item) =>
+        item.title === title ? { ...item, quantity: item.quantity - 1 } : item)
+       .filter((item) => item.quantity>0));
+  };
 return(
-    <CartContext.Provider value={{cart,addToCart}}>
+    <CartContext.Provider value={{cart,addToCart,removeFromCart}}>
         {children}
     </CartContext.Provider>
 );
