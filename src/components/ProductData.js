cac28d981529b9ab5b8e2b61da22b73a1ab184db
Shalini Gogikar <shalinigogikar23@gmail.com>
2025-02-21 15:34:49 +0530

    productData got separated

---
+++ src/components/ProductData.js
@@ -0,0 +1,44 @@
+import React from "react";
+const ProductData=[
+    {
+        id:"1",
+        title: "Colors",
+        price: 100,
+        imageUrl: ["https://prasadyash2411.github.io/ecom-website/img/Album%201.png"],
+        reviews: [
+            "Great colors! Love the quality." ,
+             "Nice, but expected more shades." 
+          ]
+      },
+      {
+        id:"2",
+        title: "Black and white Colors",
+        price: 50,
+        imageUrl: ["https://prasadyash2411.github.io/ecom-website/img/Album%202.png"],
+        reviews: [
+             "Great colors! Love the quality." ,
+             "Nice, but expected more shades." 
+          ]
+      },
+      {
+        id:"3",
+        title: "Yellow and Black Colors",
+        price: 70,
+        imageUrl: ["https://prasadyash2411.github.io/ecom-website/img/Album%203.png"],
+        reviews: [
+             "Great colors! Love the quality." ,
+              "Nice, but expected more shades." 
+          ]
+      },
+      {
+        id:"4",
+        title: "Blue Color",
+        price: 100,
+        imageUrl: ["https://prasadyash2411.github.io/ecom-website/img/Album%204.png"],
+        reviews: [
+             "Great colors! Love the quality." ,
+             "Nice, but expected more shades." 
+          ]
+      },
+];
+export default ProductData;
