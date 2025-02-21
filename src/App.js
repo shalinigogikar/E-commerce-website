bfd2244b72a585f05479358ff93d52bad7f07294
Shalini Gogikar <shalinigogikar23@gmail.com>
2025-02-21 13:36:14 +0530

    contact us imported and react-router v5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React,{useContext, useState}from "react";
-import { Route, Routes, Link,useLocation } from "react-router-dom";
+import {BrowserRouter,  Route, Switch, Link,useLocation } from "react-router-dom";
 import {Button} from "react-bootstrap";
 import { Container,Navbar,Nav } from "react-bootstrap";
 import ProductList from "./components/ProductList";
@@ -9,7 +9,16 @@ import Cart from "./components/Cart";
 import  { CartContext } from "./components/CartContext";
 import About from "./components/About";
 import Home from "./components/Home";
+import ContactUs from "./components/ContactUs";
 function App() {
+  return (
+    <BrowserRouter>
+      <MainContent />
+    </BrowserRouter>
+  );
+}
+
+function MainContent() {
   const [showCart, setShowCart] = useState(false);
   const {cart}=useContext(CartContext);
   const location=useLocation();
@@ -21,6 +30,7 @@ function App() {
       <Nav.Link as={Link} to="/">Home</Nav.Link>
       <Nav.Link as={Link} to="/store">Store</Nav.Link>
       <Nav.Link as={Link} to="/about">About</Nav.Link>
+      <Nav.Link as={Link} to="/contact">ContactUs</Nav.Link>
       </Nav>
       <Button onClick={() => setShowCart(true)}>Cart{cart.length}</Button>
       </Container>
@@ -32,16 +42,17 @@ function App() {
       <h1 className={classes.heading}>Get our Latest Album</h1><br/>
             <Button className={classes.playButton} variant="dark">►</Button>
             </div>}</header>
-    <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/store" element={<ProductList />} />
-          <Route path="/about" element={<About />} /> 
-        </Routes>
+            <Container>
+    <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/store" component={ProductList} />
+          <Route path="/about" component={About} /> 
+          <Route path="/contact" component={ContactUs}/>
+        </Switch></Container>
     {/*<ProductList/> we have moved it to store*/}
     
     <Footer/>
   </>
   );
 }
-
 export default App;
