00960dec172b05174d0e782c074252bb67499c15
Shalini Gogikar <shalinigogikar23@gmail.com>
2025-02-21 15:35:16 +0530

    view product is added

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,41 +1,28 @@
 import React, { useContext } from "react";
 import { Button, Row, Col, Container } from "react-bootstrap";
+import {Link} from "react-router-dom";
 import classes from "./Style.module.css";
 import { CartContext } from "./CartContext";
-const productsArr = [
-    {
-      title: "Colors",
-      price: 100,
-      imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
-    },
-    {
-      title: "Black and white Colors",
-      price: 50,
-      imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
-    },
-    {
-      title: "Yellow and Black Colors",
-      price: 70,
-      imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
-    },
-    {
-      title: "Blue Color",
-      price: 100,
-      imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
-    },
-  ];
+import ProductData from "./ProductData";
   const ProductList = () => {
   const { addToCart }=useContext(CartContext);
     return (
       <Container className="mt-4">
         <h2 style={{textAlign:"center"}}>Colors</h2>
         <Row>
-          {productsArr.map((product, index) => (
+          {ProductData.map((product, index) => (
             <Col key={index} md={6} lg={6} className="mb-4">
               <div>
-                <img src={product.imageUrl} alt={product.title} className="img-fluid mb-6" style={{ maxHeight: "300px",maxWidth:"200px" }}/>
+              <Link to={`/product/${product.id}`} style={{ textDecoration: "none", color: "inherit" }}>
+                <img src={product.imageUrl} 
+                alt={product.title}
+                 className="img-fluid mb-3"
+                  style={{ maxHeight: "250px",objectFit: "cover"  }}/></Link>
                   <h4>{product.title}</h4>
                   <p>Price: ${product.price}</p>
+                 <Button as={Link} to={`/product/${product.id}`} variant="info" className="me-2">
+                  View Product
+                </Button>
                   <Button variant="primary" onClick={()=>addToCart(product)}>Add to Cart</Button>
                 </div>
             </Col>
